f1cbfdba23e117744159dce9cbf58c37b1a65392
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-09-06 13:19:10 -0700

    Add global types definition file

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,2 +1,15 @@
 type Tile = 0 | 1 | null
 type GamePhase = 'rolling' | 'movement' | 'gameOver'
+
+interface PlayerI {
+  tokens: number[]
+  score: number
+  id: number
+}
+
+interface GameI {
+  players: Player[]
+  phase: GamePhase
+  activePlayer: number | null
+  //TODO
+}
