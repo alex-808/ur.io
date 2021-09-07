603561e522ddb8a9485d560083a6589288638bb7
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-09-07 15:35:42 -0700

    Improve board update method

--- client/src/game.ts
+++ client/src/game.ts
@@ -2,7 +2,7 @@ class Game {
   players: Player[] = []
   phase: GamePhase = 'rolling'
   activePlayer: number | null = null
-  board: Tile[] = [
+  board: PlayerID[] = [
     null,
     null,
     null,
@@ -34,7 +34,7 @@ class Game {
   }
   addPlayer() {
     if (this.players.length < 2) {
-      const player = new Player(this.players.length)
+      const player = new Player(this.players.length as PlayerID)
       this.players.push(player)
     } else {
       // TODO emit 'too many players'
@@ -50,42 +50,61 @@ class Game {
   }
   updateBoard() {
     for (let player of this.players) {
-      for (let token of player.tokens) {
-        //console.log({ token }, player.id)
-        if (!token) continue
-
-        const boardVal = player0Path.get(token)
-        console.log(boardVal)
-        if (boardVal) {
-          this.board[boardVal] = 1
+      const path = player.id === 0 ? player0Path : player1Path
+      for (let tokenPos of player.tokens) {
+        if (tokenPos === -1) continue
+        const boardVal = path.get(tokenPos)
+        if (boardVal !== undefined) {
+          this.board[boardVal] = player.id
         }
       }
     }
   }
 }
+
 const player0Path = new Map([
   [0, 9],
   [1, 6],
   [2, 3],
-  [4, 0],
-  [5, 1],
-  [6, 4],
-  [7, 7],
-  [8, 10],
-  [9, 13],
-  [10, 16],
-  [11, 19],
+  [3, 0],
+  [4, 1],
+  [5, 4],
+  [6, 7],
+  [7, 10],
+  [8, 13],
+  [9, 16],
+  [10, 19],
+  [11, 18],
+  [12, 15],
+  [13, 12],
 ])
 
+const player1Path = new Map([
+  [0, 11],
+  [1, 8],
+  [2, 5],
+  [3, 2],
+  [4, 1],
+  [5, 4],
+  [6, 7],
+  [7, 10],
+  [8, 13],
+  [9, 16],
+  [10, 19],
+  [11, 20],
+  [12, 17],
+  [13, 14],
+])
 class Player implements PlayerI {
-  constructor(public id: number) {}
-  tokens = [null, 10, null, 7, null, null, null]
+  constructor(public id: PlayerID) {}
+  tokens = [-1, 10, -1, 7, -1, -1, -1]
   score: number = 0
   incrementScore() {
     this.score++
   }
-  moveToken() {
-    //TODO
+  moveToken(tokenIndex: number, rollVal: number) {
+    this.tokens[tokenIndex] += rollVal
+    console.log(this.tokens)
   }
 }
 
--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -1,10 +1,10 @@
-type Tile = 0 | 1 | null
+type PlayerID = 0 | 1 | null
 type GamePhase = 'rolling' | 'movement' | 'gameOver'
 
 interface PlayerI {
-  tokens: (number | null)[]
+  tokens: number[]
   score: number
-  id: number
+  id: PlayerID
 }
 
 interface GameI {
