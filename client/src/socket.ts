6295736998730701c2fe6e8799aad1370ac5cb68
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-10-01 11:24:50 -0700

    Add environment variables to client

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,5 @@
 import io from 'socket.io-client'
-const socket = io('http://localhost:5000')
+const url = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001'
+const socket = io(url)
 
 export { socket }
