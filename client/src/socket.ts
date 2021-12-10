b53bd44d43b538fcc6574f7d5b08f1a1c7a44a8b
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-12-10 10:47:13 -0800

    Add support for development build to run locally

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,10 @@
 import io from 'socket.io-client'
-const url = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001'
+
+const url =
+  process.env.NODE_ENV === 'development'
+    ? (process.env.REACT_APP_SERVER_URL as string)
+    : 'http://localhost:3001'
+
 const socket = io(url)
 
 export { socket }
