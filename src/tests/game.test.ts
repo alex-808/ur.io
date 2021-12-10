df4956f75dedc7978d333ff9a03a4a0e5ff922a6
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2021-12-10 10:49:48 -0800

    Add Game class tests

--- src/tests/game.test.ts
+++ src/tests/game.test.ts
@@ -3,3 +3,22 @@ import { Game } from '../game';
 test('Game class to instantiate an object', () => {
   expect(typeof new Game()).toBe('object');
 });
+
+test('initial game phase is "rolling"', () => {
+  const game = new Game();
+  expect(game.phase).toBe('rolling');
+});
+
+test('.addPlayerMethod increments .players arr by 1', () => {
+  const game = new Game();
+  game.addPlayer();
+  game.addPlayer();
+  expect(game.players.length).toBe(2);
+});
+
+test('.rollDice returns an arr of two values', () => {
+  const game = new Game();
+  const rollVal = game.rollDice();
+  expect(rollVal!.length).toBe(2);
+  expect(typeof rollVal).toBe('object');
+});
